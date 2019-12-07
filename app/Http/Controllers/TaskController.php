ca45916fc6d09c40d1806957748e0284d6fd7bb2
Elias888111 <ibragimli888111@yandex.ru>
2019-12-07 17:10:38 +0400

    third commit

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,7 +6,17 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Http\Controllers\Controller;
+
 class TaskController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(Request $request)
+    {
+        return view('tasks.index');
+    }
 }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -50,8 +50,4 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 }
